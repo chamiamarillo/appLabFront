82098495507c422284fdc6bf88a706db7484f7f8
Matias Gonzalez <matias.gonzalez@live.com.ar>
2022-10-15 18:57:35 -0300

    se agrega dialog con tablas hardcode por el momento

--- src/Components/Laboratorio/Pedido.jsx
+++ src/Components/Laboratorio/Pedido.jsx
@@ -12,18 +12,30 @@ import {
   CardActionArea,
 } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const useStyles = makeStyles(() => ({
   root: {
     display: "flex",
     flexWrap: "wrap",
     "& > *": {
-      //margin: "right",
       margin: "8px",
       height: "240px"
     },
   },
 }));
+
 
 function Pedido({ pedido }) {
   const { root } = useStyles();
@@ -32,51 +44,211 @@ function Pedido({ pedido }) {
     numero_tp,
     fecha_solicitud,
     numero_laboratorio,
-    docente
+    docente, cantidad_grupos,
   } = pedido;
 
+
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState('paper');
+
+  const handleClickOpen = (scrollType) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef(null);
+
+  function createData(name, calories, fat, carbs, protein) {
+    return { name, calories, fat, carbs, protein };
+  };
+
+  const rows = [
+    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+    createData('Eclair', 262, 16.0, 24, 6.0),
+    createData('Cupcake', 305, 3.7, 67, 4.3),
+    createData('Gingerbread', 356, 16.0, 49, 3.9),
+  ];
+
   return (
-    <Box className={root}>
-      <Card style={{ backgroundColor: "#b4e0bc" }}>
-        <CardActionArea>
-          <CardHeader
-            style={{ textAlign: "left" }}
-            avatar={
-              <Avatar>
-                <AssignmentIcon />
-              </Avatar>
-            }
-            title={`Pedido número ${numero_tp}`}
-            subheader={`Fecha : ${fecha_solicitud}`}
-            action={
-              <IconButton>
-                <MoreVertIcon />
-              </IconButton>
-            }
-          />
-          <CardMedia
-            style={{ paddingTop: "3%" }}
-            image="./media/background.png"
-            title="Background image"
-          />
-          <CardContent style={{ textAlign: "left" }}>
-            <p>
-              <strong>Laboratorio: </strong> {numero_laboratorio}
-            </p>
-            <p>
-              <strong>Edificio: </strong> Malvinas
-            </p>
-            <p>
-              <strong>Docente : </strong> {`${docente.nombre} ${docente.apellido}`}
-            </p>
-            <p>
-              <strong>Estado: </strong>Aceptado
-            </p>
-          </CardContent>
-        </CardActionArea>
-      </Card>
-    </Box>
+    <div>
+
+      <Box className={root}>
+        <Card style={{ backgroundColor: "#b4e0bc" }}>
+          <CardActionArea onClick={handleClickOpen('body')}>
+            <CardHeader
+              style={{ textAlign: "left" }}
+              avatar={
+                <Avatar>
+                  <AssignmentIcon />
+                </Avatar>
+              }
+              title={`Pedido número ${numero_tp}`}
+              subheader={`Fecha : ${fecha_solicitud}`}
+              action={
+                <IconButton>
+                  <MoreVertIcon />
+                </IconButton>
+              }
+            />
+            <CardMedia
+              style={{ paddingTop: "3%" }}
+              image="./media/background.png"
+              title="Background image"
+            />
+            <CardContent style={{ textAlign: "left" }}>
+              <p>
+                <strong>Laboratorio: </strong> {numero_laboratorio}
+              </p>
+              <p>
+                <strong>Edificio: </strong> Malvinas
+              </p>
+              <p>
+                <strong>Docente : </strong> {`${docente.nombre} ${docente.apellido}`}
+              </p>
+              <p>
+                <strong>Estado: </strong>Aceptado
+              </p>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Box>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+        maxWidth="lg"
+      >
+        <DialogTitle id="scroll-dialog-title">Pedido n°: {numero_laboratorio}</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+
+            <fieldset>
+              <label for="fecha_trabajo" id="label_fecha_trabajo">Fecha solicitud : </label> <input type="text" id="fecha_trabajo" name="fecha_trabajo" value={fecha_solicitud} disabled />
+              <label for="edificio" id="label_edificio">Edificio : </label> <input type="text" id="edificio" name="edificio" value={fecha_solicitud} disabled />
+              <label for="laboratorio" id="label_laboratorio">Laboratorio : </label> <input type="text" id="laboratorio" name="laboratorio" value={numero_laboratorio} disabled />
+
+              <br></br>
+              <label id="label_docente"> docente : </label> <input type="text" id="docente" name="docente" value={`${docente.nombre}  ${docente.apellido}`} disabled />
+              <label id="label_alumno"> alumnos : </label> <input type="text" id="alumno" name="alumno" value="40" disabled />
+              <label id="label_grupo"> grupos : </label> <input type="text" id="grupo" name="grupo" value={cantidad_grupos} disabled />
+            </fieldset>
+            <hr></hr>
+            <h4>Equipos</h4>
+
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+
+            <h4>Materiales</h4>
+
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+
+
+            <h4>Reactivos</h4>
+
+            <TableContainer component={Paper}>
+              <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Dessert (100g serving)</TableCell>
+                    <TableCell align="right">Calories</TableCell>
+                    <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                    <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                    <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {rows.map((row) => (
+                    <TableRow
+                      key={row.name}
+                      sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                      <TableCell component="th" scope="row">
+                        {row.name}
+                      </TableCell>
+                      <TableCell align="right">{row.calories}</TableCell>
+                      <TableCell align="right">{row.fat}</TableCell>
+                      <TableCell align="right">{row.carbs}</TableCell>
+                      <TableCell align="right">{row.protein}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </DialogContentText>
+        </DialogContent>
+
+      </Dialog>
+
+    </div>
   );
+
 }
 
 export default Pedido;
